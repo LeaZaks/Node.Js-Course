31047eeea473bd81962d5d7d4d08343214ebee33
LeaZaks <leale1990@gmail.com>
2024-07-29 13:02:40 +0300

    Lessen 6

---
+++ Lesson 6 - 29.7.24/express_ejs_bank/middlewares/renderWithLayout.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const renderWithLayout = (layout) => {
+    return (req, res, next) =>{
+        res.renderWithLayout = (view, params = {} )=>{
+            params.body= path.join(__dirname, '../views', view)
+            res.render(layout, params);
+        }
+    next();
+    }
+
+
+}
+
+module.exports = renderWithLayout;
